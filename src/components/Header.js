e737925839d19fdc53b8bed6a87f433b5c667fcb
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-21 16:03:42 +0530

    Created Header.js Added JSX script to the header part of the web app.

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <header>
+            <div className="header-container-1">
+                <h1>Fake Store</h1>
+            </div>
+        </header>
+    );
+}
+
+
+export default Header;
