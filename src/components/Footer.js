5cc027baf4b52aeda6908f894d55568984c46d56
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-21 16:02:17 +0530

    Created Footer.js Added JSX script to the footer part of the web page.

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer>
+            <div className="footer-container">
+                <p>All rights reserved &copy;Fake Store </p>
+            </div>
+        </footer>
+    );
+}
+
+
+export default Footer;
