5d434f554118375956598e92e6e61c84e5aa7ad5
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-25 13:42:47 +0530

    Modified: Product.js Fixed bugs for no data.

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,8 @@ class Product extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            product: null
+            product: null,
+            isProduct: true
         }
     }
     componentDidMount() {
@@ -18,36 +19,45 @@ class Product extends Component {
             })
             .catch((err) => {
                 console.error(err);
+                this.setState({ isProduct: false })
             });
     }
 
     render() {
-        if (!this.state.product) {
-            return (<Loader />)
-        }
         return (
-            <div className='container-wrapper'>
-                <div className='main-container-product'>
-                    <div className="content-container-product">
-                        <img src={this.state.product.image} alt={this.state.product.title} className="img-wrapper"></img>
-                        <div className='description-container-product'>
-                            <h2>{this.state.product.title}</h2>
-                            <p>{this.state.product.description}</p>
-                            <h4>{this.state.product.category}</h4>
-                        </div>
-                    </div>
-                    <div className="container-product">
-                        <div className="price-rating-container">
-                            <div className="rating">
-                                <button className="rating-button">{this.state.product.rating.rate} <i className="fa-regular fa-star"></i></button>
-                                <span>{"(" + this.state.product.rating.count + ")"}</span>
+            !this.state.isProduct ?
+                <h1 className='fail-fetch'>Something went wrong please try again</h1>
+                :
+                (this.state.product === null) ?
+                    <Loader />
+                    :
+                    this.state.product === '' ?
+                        <h1 className='no-product'>Nothing to display</h1>
+                        :
+                        (
+                            <div className='container-wrapper'>
+                                <div className='main-container-product'>
+                                    <div className="content-container-product">
+                                        <img src={this.state.product.image} alt={this.state.product.title} className="img-wrapper"></img>
+                                        <div className='description-container-product'>
+                                            <h2>{this.state.product.title}</h2>
+                                            <p>{this.state.product.description}</p>
+                                            <h4>{this.state.product.category}</h4>
+                                        </div>
+                                    </div>
+                                    <div className="container-product">
+                                        <div className="price-rating-container">
+                                            <div className="rating">
+                                                <button className="rating-button">{this.state.product.rating.rate} <i className="fa-regular fa-star"></i></button>
+                                                <span>{"(" + this.state.product.rating.count + ")"}</span>
+                                            </div>
+                                            <h3>${this.state.product.price}</h3>
+                                        </div>
+                                        <button className="buy-now">Check-out</button>
+                                    </div>
+                                </div>
                             </div>
-                            <h3>${this.state.product.price}</h3>
-                        </div>
-                        <button className="buy-now">Check-out</button>
-                    </div>
-                </div>
-            </div>
+                        )
         );
     }
 }
