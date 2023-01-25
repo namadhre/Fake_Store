69986f622864177274df569b616da540b5454397
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-25 13:00:40 +0530

    Created Loader.js Added loader component.

---
+++ src/components/Loader.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Loader.css'
+
+class Loader extends Component {
+    render() {
+        return (
+            <div className="loader-container">
+              <div className="lds-spinner">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+        );
+    }
+}
+
+export default Loader;
