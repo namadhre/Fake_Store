f80977274622ba9a2ba4e13dbf7a9e9703fa8313
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-25 13:01:44 +0530

    Created Product.js Added Jsx script to each product.

---
+++ src/components/Product.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Loader from './Loader';
+import './Product.css'
+
+class Product extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            product: null
+        }
+    }
+    componentDidMount() {
+        let { id } = this.props.match.params;
+        axios.get(`https://fakestoreapi.com/products/${id}`)
+            .then((response) => {
+                this.setState({ product: response.data });
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    render() {
+        if (!this.state.product) {
+            return (<Loader />)
+        }
+        return (
+            <div className='container-wrapper'>
+                <div className='main-container-product'>
+                    <div className="content-container-product">
+                        <img src={this.state.product.image} alt={this.state.product.title} className="img-wrapper"></img>
+                        <div className='description-container-product'>
+                            <h2>{this.state.product.title}</h2>
+                            <p>{this.state.product.description}</p>
+                            <h4>{this.state.product.category}</h4>
+                        </div>
+                    </div>
+                    <div className="container-product">
+                        <div className="price-rating-container">
+                            <div className="rating">
+                                <button className="rating-button">{this.state.product.rating.rate} <i className="fa-regular fa-star"></i></button>
+                                <span>{"(" + this.state.product.rating.count + ")"}</span>
+                            </div>
+                            <h3>${this.state.product.price}</h3>
+                        </div>
+                        <button className="buy-now">Check-out</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Product;
