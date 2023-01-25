d5881430d27dbd19246511dfed898e861db93e05
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-25 13:02:53 +0530

    Modified: Products.js added link to each product.

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,28 +1,34 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import './Products.css';
 
 
 class Products extends Component {
     render() {
         return this.props.products.map((product) => (
-            <li key={product.id}>
-                <div className="content-container">
-                    <img src={product.image} alt={product.title}></img>
-                    <h2>{product.title}</h2>
-                    <p>{product.description}</p>
-                    <h4>{product.category}</h4>
-                </div>
-                <div className="container">
-                    <div className="price-rating-container">
-                        <div className="rating">
-                            <button className="rating-button">{product.rating.rate} <i className="fa-regular fa-star"></i></button>
-                            <span>{"(" + product.rating.count + ")"}</span>
+            <li key={product.id} >
+                <Link to={`/product/${product.id}`}>
+                    <div className="main-container-wrapper">
+                        <div className="content-container">
+                            <img src={product.image} alt={product.title}></img>
+                            <h2>{product.title}</h2>
+                            <p>{product.description}</p>
+                            <h4>{product.category}</h4>
                         </div>
-                        <h3>${product.price}</h3>
+                        <div className="container">
+                            <div className="price-rating-container">
+                                <div className="rating">
+                                    <button className="rating-button">{product.rating.rate} <i className="fa-regular fa-star"></i></button>
+                                    <span>{"(" + product.rating.count + ")"}</span>
+                                </div>
+                                <h3>${product.price}</h3>
+                            </div>
+                            <button className="buy-now">Add to Cart</button>
+                        </div>
                     </div>
-                    <button className="buy-now">Add to Cart</button>
-                </div>
+                </Link>
             </li>
+
         ));
     }
 }
