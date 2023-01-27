8d970d1acd6db0332ae9c5e239642c8b0cddd87b
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-27 09:26:38 +0530

    Modified: Header.js Added cart button in header part.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,17 @@ function Header() {
         <header>
             <div className="header-container-1">
                 <h1>Fake Store</h1>
-                <Link to="/"><button>Home</button></Link>
+                <div className="wrapper">
+                    <Link to="/">
+                        <button>Home</button>
+                    </Link>
+                    <Link to="/cart">
+                        <button className="cart-button">
+                            <i className="fa-solid fa-cart-shopping"></i>Cart
+                        </button>
+                    </Link>
+                </div>
+
             </div>
         </header>
     );
