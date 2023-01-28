0f57c692aca6b76b40797c67235a1f42d3790463
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-28 11:16:34 +0530

    Modified: App.js Refactor the code by following the best practices.

--- src/App.js
+++ src/App.js
@@ -6,48 +6,73 @@ import Loader from './components/Loader';
 
 class App extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.API_STATES = {
+      LOADING: "loading",
+      LOADED: "loaded",
+      ERROR: "error",
+    }
+
     this.state = {
       products: [],
-      isFetch: true
+      status: this.API_STATES.LOADING,
+      errorMessage: "",
     }
+
+    this.URL = 'https://fakestoreapi.com/products/'
   }
 
-  componentDidMount() {
-    axios.get('https://fakestoreapi.com/products/')
-      .then((response) => {
-        this.setState({ products: [response.data].flat() });
-      })
-      .catch((err) => {
-        this.setState({ isFetch: false });
-        console.error(err);
-      });
+  fetchData = (url) => {
+    this.setState({
+      status: this.API_STATES.LOADING,
+    }, () => {
+      axios.get(url)
+        .then((response) => {
+          this.setState({
+            status: this.API_STATES.LOADED,
+            products: response.data,
+          })
+
+        })
+        .catch((err) => {
+          this.setState({
+            status: this.API_STATES.ERROR,
+            errorMessage: "An API error occured. Please try again in a few minutes.",
+          })
+        })
+    })
+  }
+
+  componentDidMount = () => {
+    this.fetchData(this.URL);
   }
 
   render() {
+
     return (
       <div className="App">
-        {this.state.isFetch === false ?
+        {this.state.status === this.API_STATES.ERROR &&
           <div className='fetch-fail'>
-            <h2> Something went wrong !</h2>
+            <h2> {this.state.errorMessage} </h2>
           </div>
-          :
-          (this.state.products.length === 0 ?
-            <Loader />
-            :
-            (this.state.products[0] === "" ?
-              <div className="no-data">
-                <h1>No data found</h1>
-              </div>
-              :
-              <div className="main-coontainer">
-                <ul>
-                  <Products products={this.state.products} />
-                </ul>
-              </div>
-            )
-          )
+        }
+
+        {this.state.status === this.API_STATES.LOADING &&
+          <Loader />
+        }
+        {this.state.status === this.API_STATES.LOADED && this.state.products.length === 0 &&
+          <div className="no-data">
+            <h1>No products available at the moment. Please try again.</h1>
+          </div>
+        }
+        {this.state.status === this.API_STATES.LOADED && this.state.products.length > 0 &&
+          < div className="main-coontainer">
+            <ul>
+              <Products products={this.state.products} />
+            </ul>
+          </div>
         }
       </div>
     );
