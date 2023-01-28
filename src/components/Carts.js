f1cb5577d667006205757f20b440fc5559e022c1
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-28 11:18:25 +0530

    Modified: Carts.js Refactor the Carts.js by following the best practices.

--- src/components/Carts.js
+++ src/components/Carts.js
@@ -7,36 +7,67 @@ import Loader from "./Loader";
 
 
 class Carts extends Component {
-    constructor() {
-        super();
-        this.state = {
-            total: 798.04,
-            items: null,
-            isFetch: true
+    constructor(props) {
+        super(props);
+
+        this.API_STATES = {
+            LOADING: "loading",
+            LOADED: "loaded",
+            ERROR: "error",
         }
+
+        this.state = {
+            items: null,
+            status: this.API_STATES.LOADING,
+            errorMessage: "",
+        }
+
+        this.URL = 'https://fakestoreapi.com/carts/1';
+
     }
 
-    componentDidMount() {
-        axios.get('https://fakestoreapi.com/carts/1')
-            .then((response) => {
-                this.setState({ items: response.data });
-            })
-            .catch((err) => {
-                this.setState({ isFetch: false });
-                console.error(err);
-            })
+    fetchData = (url) => {
+        this.setState({
+            status: this.API_STATES.LOADING,
+        }, () => {
+            axios.get(url)
+                .then((response) => {
+                    this.setState({
+                        status: this.API_STATES.LOADED,
+                        products: response.data,
+                    })
+                })
+                .catch((err) => {
+                    this.setState({
+                        status: this.API_STATES.ERROR,
+                        errorMessage: "An API error occurred. Please try again in a few minutes.",
+                    })
+                })
+        })
+    }
+
+    componentDidMount = () => {
+        this.fetchData(this.URL)
     }
 
     render() {
+
         return (
-            this.state.isFetch === false ?
-                <div className='fetch-fail'>
-                    <h2> Something went wrong !</h2>
-                </div>
-                :
-                this.state.items === null ?
+            <>
+                {this.state.status === this.API_STATES.ERROR &&
+                    <div className='fetch-fail'>
+                        <h2>{this.state.errorMessage}</h2>
+                    </div>
+                }
+                {this.state.status === this.API_STATES.LOADING &&
                     <Loader />
-                    :
+                }
+                {this.state.status === this.API_STATES.LOADED && this.state.items === null &&
+                    <div className="no-data">
+                        <h1>No products available at the moment. Please try again later.</h1>
+                    </div>
+                }
+                {this.state.status === this.API_STATES.LOADED && this.state.items !== null &&
                     <div className="cart-wrapper">
                         <div className='main-container-body'>
                             <div className='main-right-container'>
@@ -45,8 +76,6 @@ class Carts extends Component {
                                     <p>(<span>{this.state.items.products.length}</span> )</p>
                                 </div>
                             </div>
-
-
                             {
                                 this.state.items.products.map((product) => {
                                     return (
@@ -59,7 +88,6 @@ class Carts extends Component {
                                     )
                                 })
                             }
-
                             <div className="footer-container">
                                 <Link to="/" >
                                     <button className="shopping"><i className="fa-solid fa-arrow-left"></i> Continue shopping</button>
@@ -67,8 +95,9 @@ class Carts extends Component {
                                 <button className="checkout"><i className="fa-solid fa-cart-shopping"></i><span>Checkout</span></button>
                             </div>
                         </div>
-
                     </div >
+                }
+            </>
         );
     }
 }
