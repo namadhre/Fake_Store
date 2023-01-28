18a82226560478edb443282246d849c1fdc3725b
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-28 11:19:00 +0530

    Modified: Product.js Refactor the code by following the best practices.

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,60 +4,97 @@ import Loader from './Loader';
 import './Product.css'
 
 class Product extends Component {
+
     constructor(props) {
         super(props);
+
+        this.API_STATES = {
+            LOADING: "loading",
+            LOADED: "loaded",
+            ERROR: "error",
+        }
+
         this.state = {
             product: null,
-            isProduct: true
+            status: this.API_STATES.LOADING,
+            errorMessage: "",
         }
+
+        this.URL = `https://fakestoreapi.com/products/${this.props.match.params.id}`;
     }
-    componentDidMount() {
-        let { id } = this.props.match.params;
-        axios.get(`https://fakestoreapi.com/products/${id}`)
-            .then((response) => {
-                this.setState({ product: response.data });
-            })
-            .catch((err) => {
-                console.error(err);
-                this.setState({ isProduct: false })
-            });
+
+    fetchData = (url) => {
+        this.setState({
+            status: this.API_STATES.LOADED,
+        }, () => {
+            axios.get(url)
+                .then((response) => {
+                    this.setState({
+                        status: this.API_STATES.LOADED,
+                        product: response.data,
+                    })
+
+
+                })
+                .catch((err) => {
+                    this.setState({
+                        status: this.API_STATES.ERROR,
+                        errorMessage: "An An API error occurred. Please try again in a few minutes.",
+                    })
+                })
+        })
+    }
+
+    componentDidMount = () => {
+        this.fetchData(this.URL);
     }
 
     render() {
+
         return (
-            !this.state.isProduct ?
-                <h1 className='fail-fetch'>Something went wrong please try again</h1>
-                :
-                (this.state.product === null) ?
+            <>
+
+                {this.state.status === this.API_STATES.ERROR &&
+                    <h1 className='fail-fetch'>
+                        {this.state.errorMessage}
+                    </h1>
+                }
+
+                {this.state.status === this.API_STATES.LOADING &&
                     <Loader />
-                    :
-                    this.state.product === '' ?
-                        <h1 className='no-product'>Nothing to display</h1>
-                        :
-                        (
-                            <div className='container-wrapper'>
-                                <div className='main-container-product'>
-                                    <div className="content-container-product">
-                                        <img src={this.state.product.image} alt={this.state.product.title} className="img-wrapper"></img>
-                                        <div className='description-container-product'>
-                                            <h2>{this.state.product.title}</h2>
-                                            <p>{this.state.product.description}</p>
-                                            <h4>{this.state.product.category}</h4>
-                                        </div>
-                                    </div>
-                                    <div className="container-product">
-                                        <div className="price-rating-container">
-                                            <div className="rating">
-                                                <button className="rating-button">{this.state.product.rating.rate} <i className="fa-regular fa-star"></i></button>
-                                                <span>{"(" + this.state.product.rating.count + ")"}</span>
-                                            </div>
-                                            <h3>${this.state.product.price}</h3>
-                                        </div>
-                                        <button className="buy-now">Check-out</button>
-                                    </div>
+                }
+
+                {this.state.status === this.API_STATES.LOADED && this.state.product === "" &&
+                    <h1 className='no-product'>No products available at the moment. Please try again later.</h1>
+                }
+                {this.state.status === this.API_STATES.LOADED && this.state.product !== "" && this.state.product !== null &&
+                    <div className='container-wrapper'>
+                        <div className='main-container-product'>
+                            <div className="content-container-product">
+                                <img src={this.state.product.image} alt={this.state.product.title} className="img-wrapper"></img>
+                                <div className='description-container-product'>
+                                    <h2>{this.state.product.title}</h2>
+                                    <p>{this.state.product.description}</p>
+                                    <h4>{this.state.product.category}</h4>
                                 </div>
                             </div>
-                        )
+                            <div className="container-product">
+                                <div className="price-rating-container">
+                                    <div className="rating">
+                                        <button className="rating-button">{this.state.product.rating.rate} <i className="fa-regular fa-star"></i></button>
+                                        <span>{"(" + this.state.product.rating.count + ")"}</span>
+                                    </div>
+                                    <h3>${this.state.product.price}</h3>
+                                </div>
+                                <button className="buy-now">Check-out</button>
+                            </div>
+                        </div>
+                    </div>
+                }
+
+            </>
+
+
         );
     }
 }
